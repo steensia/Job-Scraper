c31f773463b6a2bd9984e18527bea1e23545966a
steensia <35542059+steensia@users.noreply.github.com>
2021-10-26 16:43:24 -0600

    Finished project, just need to deploy to AWS Lambda for scheduled cron

--- job-scraper.py
+++ job-scraper.py
@@ -11,22 +11,23 @@ from twilio.rest import Client
 
 
 class JobScraper:
-    """
-        Initialize Selenium Chrome Driver and BeautifulSoup objects
-    """
-
     def __init__(self):
+        """
+        Initialize job list, Selenium Chrome Driver, and BeautifulSoup objects
+        """
         self.URL = 'https://www.levels.fyi/still-hiring/'
 
         # Initialize main objects
+        self.jobs = []
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.soup = BeautifulSoup(self.filter_website(), "html.parser")
 
-    """ Go to website and filter by SWE jobs
-        Return the page source for soup to parse
-    """
-
     def filter_website(self):
+        """
+        Go to website and filter by SWE jobs
+        Return the page source for soup to parse
+        :return:
+        """
         # Navigate to the link
         self.driver.get(self.URL)
 
@@ -47,12 +48,14 @@ class JobScraper:
         return page_source
 
     def scrape_jobs(self):
+        """
+        Web scrape for new jobs and send notifications
+        """
         # Retrieve the table of job listings
         table = self.soup.find("table", attrs={"class": "hiring-companies-table"})
 
         # Iterate through each row and grab data except column headers row
-        job_list = []
-        for row in table.find_all("tr")[1:14]:
+        for row in table.find_all("tr")[1:]:
             # Extract valid job listing values, otherwise ignore
             try:
                 company = row.find("th").text.strip()
@@ -61,118 +64,113 @@ class JobScraper:
 
             data = row.find_all("td")[1:]
 
-            locations = data[0].text.strip().split(' ')
-            new_locations = []
-            for location in locations:
-                if "UT" in location and "Utah" not in new_locations:
-                    new_locations.append("Utah")
-                elif "Remote" in locations and "Remote" not in new_locations:
-                    new_locations.append("Remote")
-
-            #print(new_locations)
-
-            #locations = [location.strip() for location in locations if "UT" in location or "Remote" in location]
-            #print(locations)
+            locations = self.__parse_locations(data[0].text.strip().split(','))
 
             # Show new job listings in Utah or Remote only
-            if not new_locations:
-                None
-                #continue
+            if not locations:
+                continue
 
             # Parse out date and format to mm/dd/yyyy
-            date = parse_date(data[1].text.strip().split(' '))
+            date = self.__parse_date(data[1].text.strip().split(' '))
 
             url = data[2].find("a")['href']
 
             # Only show new job listings
-            if new_jobs(date, 14):
-                job_list.append([company, ', '.join(new_locations), datetime.strftime(date, "%m/%d/%Y"), url])
+            if self.__is_new(date, days_before_today=0):
+                self.jobs.append([company, ', '.join(locations), datetime.strftime(date, "%m/%d/%Y"), url])
 
-        return job_list
+        # Send notifications if there are new jobs available
+        if self.jobs:
+            self.send_text()
+            self.send_email()
 
+    def send_email(self):
+        """
+        Send email to apply to new job(s)
+        """
 
-def new_jobs(date, days_back):
-    """
-    Helper method to parse and format date to mm/dd/yyyy
-    :param days_back:
-    :param date:
-    :return:
-    """
-    today = datetime.combine(datetime.today(), datetime.min.time()) - timedelta(days_back)
+        html = self.__display_data(self.jobs).to_html()
+        message = Mail(From("steensia101@gmail.com"),
+                       To("steensia101@gmail.com"),
+                       Subject("Levels.fyi Job Alerts"),
+                       PlainTextContent("Your job alert for Software Engineer"),
+                       HtmlContent("<h2>Your job alert for software engineer</h2>" + html))
+        try:
+            sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+            sg.client.mail.send.post(request_body=message.get())
+            print("Email sent successfully")
+        except Exception as e:
+            print(e)
 
-    return date >= today
+    def send_text(self):
+        """
+        Send text alert to notify of new job(s)
+        """
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        client = Client(account_sid, auth_token)
 
+        try:
+            client.messages \
+                .create(body="{} new Software Engineer jobs from Levels.fyi".format(len(self.jobs)),
+                        from_='+13852179269',
+                        to='+18019797937')
+            print("Text sent successfully")
+        except Exception as e:
+            print(e)
 
-def parse_date(date):
-    """
-    Return true if new jobs are available, false otherwise
-    :param date:
-    :return:
-    """
-    month = datetime.strptime(date[0], '%b').month
-    day = int(date[1])
-    year = int(date[2]) if len(date) > 2 else datetime.today().year
+    def __is_new(self, date, days_before_today):
+        """
+        Return true if new jobs are available, false otherwise
+        :param days_before_today:
+        :param date:
+        :return:
+        """
+        today = datetime.combine(datetime.today(), datetime.min.time()) - timedelta(days_before_today)
 
-    return datetime(year=year, month=month, day=day)
+        return date >= today
 
+    def __parse_locations(self, locations):
+        """
+        Helper function to parse and return locations: Remote/Utah
+        :param locations:
+        :return:
+        """
+        new_locations = set()
+        for location in set(locations):
+            if "Remote" in location:
+                new_locations.add("Remote")
+            elif "UT" in location:
+                new_locations.add("Utah")
+        return new_locations
 
-def display_data(job_list):
-    """
-    Helper function ti print out data using pandas
-    :param job_list:
-    """
-    # TODO: Perform data export
-    pd.set_option('display.max_rows', 500)
-    pd.set_option('display.max_columns', 500)
-    pd.set_option('display.width', 1000)
+    def __parse_date(self, date):
+        """
+        Helper function to parse and format date to mm/dd/yyyy
+        :param date:
+        :return:
+        """
+        month = datetime.strptime(date[0], '%b').month
+        day = int(date[1])
+        year = int(date[2]) if len(date) > 2 else datetime.today().year
 
-    df = pd.DataFrame(job_list, columns=["Company", "Locations", "Date", "Link/Email"])
-    if not df.empty:
-        print(df)
+        return datetime(year=year, month=month, day=day)
 
-    return df
+    def __display_data(self, job_list):
+        """
+        Helper function to print out data in a formatted table
+        :param job_list:
+        """
+        pd.set_option('display.max_rows', 500)
+        pd.set_option('display.max_columns', 500)
+        pd.set_option('display.width', 1000)
 
+        df = pd.DataFrame(job_list, columns=["Company", "Location(s)", "Date", "Link"])
+        # print(df)
 
-def send_email(job_list):
-    """
-    Send email to apply to new job(s)
-    """
-
-    html = display_data(job_list).to_html()
-    message = Mail(From("steensia101@gmail.com"),
-                   To("steensia101@gmail.com"),
-                   Subject("Levels.fyi Job Alerts"),
-                   PlainTextContent("Your job alert for Software Engineer"),
-                   HtmlContent(html))
-    try:
-        sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-        sg.client.mail.send.post(request_body=message.get())
-        print("Email sent successfully")
-    except Exception as e:
-        print(e.body)
-
-
-def send_text(job_list):
-    """
-    Send text alert to notify of new job(s)
-    """
-    account_sid = os.environ['TWILIO_ACCOUNT_SID']
-    auth_token = os.environ['TWILIO_AUTH_TOKEN']
-    client = Client(account_sid, auth_token)
-
-    try:
-        client.messages \
-            .create(body="{} new Software Engineer jobs from Levels.fyi".format(len(job_list)),
-                    from_='+13852179269',
-                    to='+18019797937')
-        print("Text message sent successfully")
-    except Exception as e:
-        print(e)
+        return df
 
 
 if __name__ == '__main__':
     js = JobScraper()
-    jobs = js.scrape_jobs()
-    display_data(jobs)
-    # send_text(jobs)
-    # send_email(jobs)
+    js.scrape_jobs()
